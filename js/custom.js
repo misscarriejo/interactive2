70adc9014029db2df81ffc1b32961a1f5d968d12
misscarriejo <Carrie@Air2s-MacBook-Air.local>
2016-10-04 15:44:04 -0400

    map attempt
    put in the custom.js, need to add pin and re-center the location

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,21 @@
+/*function myMap() {
+    var myCenter = new google.maps.LatLng(47.551719,-122.041122);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {center: myCenter, zoom: 5};
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+} 
+
+Yeah this one didn't work.
+*/
+
+
 $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var location = new google.maps.LatLng(47.551719,-122.041122);
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
