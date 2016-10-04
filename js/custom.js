717965e10418b353c85439dc674b3ac1185848f0
misscarriejo <Carrie@Air2s-MacBook-Air.local>
2016-10-04 15:52:43 -0400

    updated map coordinates
    also fixed h2 weight to 700. need to add in the pin.

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var location = new google.maps.LatLng(47.550591, -122.049826);
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
@@ -17,3 +17,5 @@ $(function () {
 
     google.maps.event.addDomListener(window, 'load', initMap);
 });
+
+/* Add in the pin */
