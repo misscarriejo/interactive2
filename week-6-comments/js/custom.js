df27e8129a2f458387892de632c879a24500ec10
misscarriejo <Carrie@Air2s-MacBook-Air.local>
2016-10-24 15:04:43 -0400

    organized all comments
    organized by week, including their css. now have a working record

---
+++ week-6-comments/js/custom.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(47.550591, -122.049826);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
+
+/* Add in the pin */
